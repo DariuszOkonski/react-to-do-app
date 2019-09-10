728fcb38f5c5799cbd5cd85c2cc63bc4bcdc1a17
Dariusz Okonski <darek200180@gmail.com>
2019-09-10 22:02:43 +0100

    add Content and Footer components and scss styles for it

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Header />
+        <Content />
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__paragraph">
+        ToDo - App &#169; All Rights Reserved
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
