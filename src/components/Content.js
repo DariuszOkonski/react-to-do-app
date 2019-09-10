5c8043a7223a901e4411d80ba07d2a9f1f4d3e27
Dariusz Okonski <darek200180@gmail.com>
2019-09-10 22:14:45 +0100

    prepare content with flexbox

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Content = () => {
   return (
     <main className="content">
-      <h1>Content</h1>
+
+      <div className="content__container">
+        <h1>Content</h1>
+      </div>
+
     </main>
   );
 }
