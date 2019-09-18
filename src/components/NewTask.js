8949778f452d86b0d81188d12842938b09e20f26
Dariusz Okonski <darek200180@gmail.com>
2019-09-18 13:46:54 +0100

    implement funcionality for all components at NewTask component

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,24 +1,106 @@
 import React from 'react';
 
-const NewTask = () => {
-  return (
-    <div className="new-task">
-      <h1 className="new-task__header">New task</h1>
+const minDate = new Date().toISOString().substr(0, 10);
+const maxDate = new Date().getFullYear() + 2 + '-12-31';
 
-      <form className="new-task__form form">
-        <label htmlFor="task" className="form__label">Task</label>
-        <input type="text" name="task" className="form__input" />
+class NewTask extends React.Component {
+  state = {
+    text: '',
+    priority: false,
+    date: '',
+  }
 
-        <label htmlFor="priority" className="form__label">Priority</label>
-        <input type="checkbox" name="priority" className="form__checkbox" />
+  handleTextChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    })
+  }
 
-        <label htmlFor="deadline" className="form__label">Deadline</label>
-        <input type="number" name="deadline" className="form__number" />
+  handlePriorityChange = () => {
+    this.setState(prevState => ({
+      priority: !prevState.priority,
+    }))
+  }
 
-        <input type="submit" value="Add New Task" className="form__submit" />
-      </form>
-    </div>
-  );
+  handleDataChange = (e) => {
+    this.setState({
+      date: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.text.length === 0) {
+      alert('Insert some task to do...');
+      return;
+    }
+
+    if (this.state.date.length === 0) {
+      alert('Choose deadline date');
+      return;
+    }
+
+    console.log(this.state.text, this.state.priority, this.state.date);
+
+    this.setState({
+      text: '',
+      priority: false,
+      date: minDate,
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      date: minDate,
+    })
+  }
+
+  render() {
+    const { text, priority, date } = this.state;
+
+    return (
+      <div className="new-task">
+        <h1 className="new-task__header">New task</h1>
+
+        <form onSubmit={this.handleSubmit} className="new-task__form form">
+          <label htmlFor="task" className="form__label">Task</label>
+          <input
+            maxLength="100"
+            type="text"
+            name="task"
+            className="form__input"
+            value={text}
+            onChange={this.handleTextChange}
+          />
+
+          <label htmlFor="priority" className="form__label">Priority</label>
+          <input
+            type="checkbox"
+            name="priority"
+            className="form__checkbox"
+            checked={priority}
+            onChange={this.handlePriorityChange}
+          />
+
+          <label htmlFor="deadline" className="form__label">Deadline</label>
+          <input
+            type="date"
+            name="deadline"
+            className="form__number"
+            min={minDate}
+            max={maxDate}
+            value={date}
+            onChange={this.handleDataChange}
+          />
+
+          <input type="submit" value="Add New Task" className="form__submit" />
+        </form>
+      </div>
+    );
+  }
+
+
 }
 
 export default NewTask;
